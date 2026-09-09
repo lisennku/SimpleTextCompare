bce95f471058e71a6142b7998582790247f4cedd
lcza <245062522@qq.com>
2026-09-09 09:19:52 +0800

    simple text compare cli add doc

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,12 @@
+//! 结构体`Cli`用于命令行参数
+//! - `left_file` 用于指定左文件
+//! - `right_file` 用于指定右文件
+//! - `path` 用于指定左右文件相同的路径，可选
+//!
 use clap::Parser;
 #[derive(Parser, Debug)]
 #[command(version, about)]
-pub struct Cli{
+pub struct Cli {
     pub left_file: String,
     pub right_file: String,
     #[arg(short, long)]
