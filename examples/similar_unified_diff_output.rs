4cba4c4ce36c253b024f79bdaf99174c4fe53946
lcza <245062522@qq.com>
2026-09-21 16:40:22 +0800

    stc examples update

--- examples/similar_unified_diff_output.rs
+++ examples/similar_unified_diff_output.rs
@@ -12,9 +12,11 @@ fn main() {
     let diff = TextDiff::from_lines(&left_str, &right_str);
 
     let mut unified_diff = diff.unified_diff();
-    unified_diff.context_radius(4);
-    for hunk in unified_diff.iter_hunks() {
-        println!("{}", hunk.header());
-        println!("{:?}", hunk.ops());
-    }
+    unified_diff.context_radius(4).header("left", "right");
+
+    println!("{}", unified_diff);
+    // for hunk in unified_diff.iter_hunks() {
+    //     println!("{}", hunk.header());
+    //     println!("{:?}", hunk.ops());
+    // }
 }
